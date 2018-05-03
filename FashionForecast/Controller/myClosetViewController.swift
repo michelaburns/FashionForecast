57afb2f75d79763020b24cd9040f52450fc206e1
Michela Burns <burnsmimi@gmail.com>
2018-05-03 00:42:00 -0700

    Committing again because last time the myClosetViewController didn't work.

--- FashionForecast/Controller/myClosetViewController.swift
+++ FashionForecast/Controller/myClosetViewController.swift
@@ -2,23 +2,58 @@
 //  myClosetViewController.swift
 //  FashionForecast
 //
-//  Created by Owner on 5/3/18.
+//  Created by Emily Koagedal on 5/2/18.
 //  Copyright © 2018 Michela. All rights reserved.
 //
 
 import UIKit
 
-class myClosetViewController: UIViewController {
+class imageCell: UICollectionViewCell {
+    @IBOutlet weak var closetImage: UIImageView!
+    
+}
+
+class myClosetViewController: UIViewController, UICollectionViewDelegate, UICollectionViewDataSource {
+    
+    @IBOutlet weak var collectionView: UICollectionView!
+    var myPics: [UIImage] = []
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        collectionView.dataSource = self
+        collectionView.delegate = self
+        myPics = myCloset
+        
+        print(myPics.count)
+       
 
         // Do any additional setup after loading the view.
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        self.collectionView!.reloadData()
+        print("HERE")
+        myPics = myCloset
+        
+        print(myPics.count)
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    //Number of views
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return myPics.count
+    }
+    
+    //Populate views
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "imageCell", for: indexPath) as! imageCell
+        cell.closetImage.image = myPics[indexPath.row]
+        return cell
     }
     
 
